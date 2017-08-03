6aa7419b3c3cee57dde443528eabeb12491dcb6d
liweijian <liweijian@yingu.com>
2017-08-03 11:14:59 +0800

    添加配置仓库

--- feign-consumer/src/main/java/com/sayabc/controller/ConsumerController.java
+++ feign-consumer/src/main/java/com/sayabc/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.sayabc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,16 @@ public class ConsumerController {
     @Autowired
     private ComputeClient computeClient;
 
+    @Value("${client}")
+    public String client;
+
     @GetMapping("/add")
     public Integer add() {
         return computeClient.add(10, 20);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return this.client;
     }
 }
